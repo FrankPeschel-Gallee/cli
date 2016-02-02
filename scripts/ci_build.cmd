079f6963250b1c7f5ff741eeec6656edc9ec0005
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-02-02 10:04:50 -0800

    working on build scripts

--- scripts/ci_build.cmd
+++ scripts/ci_build.cmd
@@ -4,7 +4,11 @@ REM Copyright (c) .NET Foundation and contributors. All rights reserved.
 REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 set CI_BUILD=1
+set CONFIGURATION=%1
+set VERBOSE=1
 
-CALL %~dp0..\build.cmd %*
+set DOTNET_BUILD_SKIP_CROSSGEN=1
+
+CALL %~dp0..\build.cmd %2
 
 exit /b %errorlevel%
